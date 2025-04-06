cc4b1960bbbc61878eb73925738a8b683a76bf8b
zhejun su <suzhejunk9@gmail.com>
2025-04-06 05:34:47 -0400

    add lobby page functionality and styling

--- src/components/lobby.jsx
+++ src/components/lobby.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Lobby = () => {
-  const [allLobby, setAllLobby] = useState([])
+  const [allLobby, setAllLobby] = useState([]);
+  const [isButtonVisible, setISButtonVisible] = useState("");
+  const [isInputVisible, setIsInputVisible] = useState("none");
+  const [partyName, setPartyName] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getAllRoom = async() => {
@@ -12,20 +17,59 @@ const Lobby = () => {
     getAllRoom()
   },[])
 
+  const getInputBar = () => {
+    setISButtonVisible("none");
+    setIsInputVisible("");
+  }
+
+  const createLobby = async(event) => {
+    event.preventDefault()
+    const res = await fetch('https://pictionary-183l.onrender.com/lobby', {
+      method: `POST`,
+      headers: {
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify({
+        name:partyName
+      })
+    });
+    if (res.ok) {
+      const newLobby = await res.json()
+      navigate(`/lobby/${newLobby.id}`)
+    }
+    else {
+      alert("please enter a valid party name")
+    }
+  }
+
   return (
     <>
-      <h1>All Lobbies</h1>
+    <h1>All Lobbies</h1>
+    <section id="alllobby">
       <section>
+        <button style={{display:isButtonVisible}} onClick={getInputBar}>Create Lobby</button>
+        <form style={{display:isInputVisible}} id="createroom" onSubmit={createLobby}>
+          <label>Party Name :</label> <input onChange={(e)=>{setPartyName(e.target.value)}}></input>
+          <button id="createroom">Create</button>
+        </form>
+      </section>
+      <h3 id="or">OR</h3>
+      <section id="lobbylist">
+        <h3>Join a Lobby</h3>
         {
           allLobby.map((singleLobby) => {
+            const goSingleLobby = () => {
+              navigate(`/lobby/${singleLobby.id}`)
+            }
             return (
-              <section key={singleLobby.id}>
+              <section key={singleLobby.id} onClick={goSingleLobby}>
                 <ul>{singleLobby.name}</ul>
               </section>
             )
           })
         }
       </section>
+    </section>
     </>
   )
 }
